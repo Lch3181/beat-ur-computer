7982ffb112a4e56ec1113d2cb72d0e24d2725abb
Jacob Frost <36671442+frosty656@users.noreply.github.com>
2019-03-25 13:43:10 -0400

    particle effects

--- Assets/Scripts/Collision.cs
+++ Assets/Scripts/Collision.cs
@@ -2,48 +2,82 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Collision : MonoBehaviour
 {
     public AudioSource collisionSound;
-    public ParticleSystem effect;
+    public GameObject effect;
+    public ParticleSystem particleSys;
     public Rigidbody rb;
+
+    float delay = 1f;
 
     //[Tooltip("Smallest velocity for sound to play")]
 
     // public float impactVelocity;
 
+    private void Update()
+    {
+        delay -= Time.deltaTime;
+    }
+
+
     private void OnCollisionEnter(UnityEngine.Collision collision)
     {
-
         float velocity = rb.velocity.magnitude;
 
-        
+         if (velocity >  1)
+         {
+        print("PLaying effect");
+            if(effect != null)
+                Instantiate(effect, rb.position, transform.rotation);
 
+            if (particleSys != null)
+            {
+                //Instantiate(particleSys, rb.position, transform.rotation);
+                particleSys.Play();
+                delay = 7f;
+            }
+
+
+        }
+
+
+
+
+        collisionSound.pitch = Random.Range(0.2f, .8f);
 
         print("Velocity = " + rb.velocity.magnitude);
+        print("Pitch = " + collisionSound.pitch);
 
-        if (velocity > 0 && velocity < 1)
-        {
-            collisionSound.volume = .10f;
-        }
-        if (velocity > 1 && velocity < 5)
-        {
-            collisionSound.volume = .35f;
-        }
-        if (velocity > 5 && velocity < 8)
-        {
-            collisionSound.volume = .55f;
-        }
-        if (velocity > 8 && velocity < 20)
-        {
-            collisionSound.volume = .75f;
-        }
-        if (velocity > 20)
-        {
-            collisionSound.volume = 1f;
-        }
 
-        collisionSound.Play();
+        //if (velocity > 0 && velocity < 1)
+        //{
+        //    collisionSound.volume = .10f;
+        //}
+        //if (velocity > 1 && velocity < 5)
+        //{
+        //    collisionSound.volume = .35f;
+        //}
+        //if (velocity > 5 && velocity < 8)
+        //{
+        //    collisionSound.volume = .55f;
+        //}
+        //if (velocity > 8 && velocity < 20)
+        //{
+        //    collisionSound.volume = .75f;
+        //}
+        //if (velocity > 20)
+        //{
+        //    collisionSound.volume = 1f;
+        //}
+
+        if (velocity > 1)
+        {
+            
+            collisionSound.Play();
+            
+        }
 
     }
 }
