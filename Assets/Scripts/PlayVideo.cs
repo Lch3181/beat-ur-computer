1f3acc023784e2a2f349f2dc15e5729bfe8213db
Mitchell7583 <lamchniho@gmail.com>
2019-04-18 16:07:36 -0400

    added error screen

--- Assets/Scripts/PlayVideo.cs
+++ Assets/Scripts/PlayVideo.cs
@@ -5,20 +5,25 @@ using UnityEngine.Video;
 
 public class PlayVideo : MonoBehaviour
 {
-    private int stage = 2;
+    public int stage = 2;
     private double delay = 0;
+    private Random random = new Random();
     Transform LoginScreen;
     Transform wallpaper;
+    Transform ErrorScreen;
     Transform Startup;
     Transform Shutdown;
+    Transform BlueScreen;
 
     // Start is called before the first frame update
     void Start()
     {
         LoginScreen = gameObject.transform.Find("LoginScreen");
         wallpaper = gameObject.transform.Find("wallpaper");
+        ErrorScreen = gameObject.transform.Find("GreenScreenError");
         Startup = gameObject.transform.Find("WindowsXP startup");
         Shutdown = gameObject.transform.Find("WindowsXP shutdown");
+        BlueScreen = gameObject.transform.Find("BlueScreen");
     }
 
     // Update is called once per frame
@@ -32,6 +37,20 @@ public class PlayVideo : MonoBehaviour
             Startup.gameObject.SetActive(false);
             LoginScreen.gameObject.SetActive(true);
             stage = 2;
+        }
+        //Green screen error loop after login at a random time and disable shutdown
+        if(delay<=0 && wallpaper.gameObject.activeSelf && !ErrorScreen.gameObject.activeSelf)
+        {
+            wallpaper.gameObject.SetActive(false);
+            ErrorScreen.gameObject.SetActive(true);
+            delay = 4.5f;
+            stage = -1;
+        }
+        //blue screen after green screen is done
+        if(delay <= 0 && ErrorScreen.gameObject.activeSelf && !ErrorScreen.gameObject.GetComponent<VideoPlayer>().isPlaying)
+        {
+            ErrorScreen.gameObject.SetActive(false);
+            BlueScreen.gameObject.SetActive(true);
         }
         //reset stage after shutdown
         if(delay <= 0 && Shutdown.gameObject.activeSelf && !Shutdown.gameObject.GetComponent<VideoPlayer>().isPlaying)
@@ -66,6 +85,7 @@ public class PlayVideo : MonoBehaviour
             case 2://display wallpaper
                 LoginScreen.gameObject.SetActive(false);
                 wallpaper.gameObject.SetActive(true);
+                delay = Random.Range(3f, 8f);
                 stage++;
                 break;
             case 3://shutdown
